e19ba8efd9f5b5c27e3160c91badb5f0f9cddbc7
MarwanTushyeh <marwan.tushyeh@gmail.com>
2016-08-11 20:29:05 +0300

    FirstCommit

---
+++ tracker/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+
+ 
+class RunningSession(models.Model):
+    """Development iteration period."""
+
+    id = models.IntegerField(primary_key=True)
+    name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    duration = models.FloatField()
+    miles = models.FloatField()
+    createdDate = models.DateTimeField(auto_now_add=True, blank=True)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+ 
+# Create your models here.
---
+++ tracker/serializers.py
@@ -0,0 +1,29 @@
+
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+from .models import RunningSession
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+    
+
+    class Meta:
+        model = User
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', )
+   
+
+        
+class RunSessionSerializer(serializers.ModelSerializer):
+       
+        class Meta:
+           model = RunningSession
+           fields = ('createdDate', 'miles', 'duration', 'id',)
+           read_only_fields = ('id',)
+
+   
